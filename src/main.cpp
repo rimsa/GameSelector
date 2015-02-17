1aac21f1de08f800639ec5ed9d4d70283d008d07
Andrei Rimsa <rimsa@live.com>
2015-02-17 16:37:23 -0200

    Opening a dialog to select directory if no argument is provided.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,43 @@
 #include <QDebug>
 
+#include <QMessageBox>
+#include <QFileDialog>
 #include <QApplication>
 #include <GameSelector/GameSelector.h>
+
+void showInvalidDirectory() {
+    QMessageBox::critical(0, "Game Selector",
+                             "Invalid directory",
+                             QMessageBox::Ok);
+}
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
-    QDir root = (argc == 2 ? QDir(argv[1]) : QDir::current());
-    if (!root.exists())
-        qDebug() << "Invalid directory";
+
+    QString rootDir;
+    if (argc == 2) {
+        rootDir = QString(argv[1]);
+    } else {
+        QDir current(qApp->applicationDirPath());
+#ifdef Q_OS_OSX
+        current.cdUp();
+        current.cdUp();
+#endif
+
+        rootDir = QFileDialog::getExistingDirectory(0, "Select game root folder", current.absolutePath());
+    }
+
+    if (rootDir.isEmpty()) {
+        showInvalidDirectory();
+        return -1;
+    }
+
+    QDir root(rootDir);
+    if (!root.exists()) {
+        showInvalidDirectory();
+        return -1;
+    }
 
     GameSelector w(root);
     Q_UNUSED(w);
